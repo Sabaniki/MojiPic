bcac685e04cdd4118748a02020b36c4c0d6bb245
Sabaniki <sawada13@mccc.jp>
2020-03-18 16:36:56 +0900

    webページ部分が完成したらしい。マジ？？←マジです。多分。

--- app/views/index.js
+++ app/views/index.js
@@ -3,3 +3,10 @@ import "materialize-css";
 import Dropzone from "dropzone";
 import React from "react"
 import ReactDOm from "react-dom";
+
+// Materializedの設定
+$(
+    function () {
+        $(".button-collapse").sideNav();
+    }
+);  // end of document ready
